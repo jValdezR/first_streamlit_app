1c66605db1d6f82d5e4b2f4188e9da43b80efc09
Jesus Daniel Valdez Rodriguez <37342850+jValdezR@users.noreply.github.com>
2023-06-21 19:09:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner');
+
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 & Blueberry Oatmeal');
+streamlit.text('Kale, Spinach & Rocket Smoothie');
+streamlit.text('Hard-Boiled Free-Range Egg');
